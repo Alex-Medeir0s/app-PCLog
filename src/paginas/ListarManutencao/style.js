9fb77b98139c24d219a39b2db2a1148c5c0cecef
Alex-Medeir0s <alexmedeiros157@outlook.com>
2025-06-27 09:39:16 -0300

    feat: Atualizar estilos e estrutura do componente de manutenção para melhor usabilidade

--- src/paginas/ListarManutencao/style.js
+++ src/paginas/ListarManutencao/style.js
@@ -1,76 +1,82 @@
+// style.js (ListarManutencao Mobile)
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f2f2f2",
-    padding: 16,
+    backgroundColor: "#f0f4f8",
+    paddingHorizontal: 16,
+    paddingTop: 24,
+  },
+  filtroContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 10,
+  },
+  filtroTexto: {
+    fontSize: 15,
+    color: "#777",
+    fontWeight: "600",
+  },
+  filtroAtivo: {
+    color: "#007bff",
+    textDecorationLine: "underline",
+  },
+  filtroConcluido: {
+    color: "green",
+    textDecorationLine: "underline",
+  },
+  filtroPendente: {
+    color: "red",
+    textDecorationLine: "underline",
   },
   card: {
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    padding: 16,
+    marginBottom: 12,
     flexDirection: "row",
     justifyContent: "space-between",
-    backgroundColor: "#fff",
-    padding: 16,
-    marginBottom: 10,
-    borderRadius: 8,
-    elevation: 2,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 4,
   },
   cardTitle: {
-    fontSize: 18,
+    fontSize: 17,
     fontWeight: "bold",
     color: "#333",
   },
   cardSubtitle: {
     fontSize: 14,
-    color: "#666",
+    color: "#555",
   },
   cardDate: {
     fontSize: 12,
     color: "#999",
+    marginBottom: 4,
   },
   emptyText: {
     textAlign: "center",
-    marginTop: 20,
+    color: "#999",
     fontSize: 16,
-    color: "#888",
+    marginTop: 50,
   },
   bottomButton: {
     position: "absolute",
-    bottom: 50, // <- afastado da borda inferior
+    bottom: 20,
     left: 20,
     right: 20,
     backgroundColor: "#007bff",
     paddingVertical: 14,
     borderRadius: 12,
     alignItems: "center",
-    justifyContent: "center",
-    elevation: 4,
   },
   bottomButtonText: {
-    color: "#fff",
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
-    fontWeight: "bold",
   },
-    filtroContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginBottom: 12,
-  },
-  filtroTexto: {
-    fontSize: 16,
-    color: "#666",
-  },
-  filtroAtivo: {
-    color: "#007bff",
-    fontWeight: "bold",
-  },
-  filtroConcluido: {
-    color: "green",
-    fontWeight: "bold",
-  },
-  filtroPendente: {
-    color: "red",
-    fontWeight: "bold",
-  },
-
 });
